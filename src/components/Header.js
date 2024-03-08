0fcd5363d53f627d339747063c1a3285a60305e4
Ubuntu <ubuntu@ip-172-31-17-88.ap-south-2.compute.internal>
2024-03-08 18:09:39 +0000

    rontend reactjs initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header className="header">
+      <nav className="nav">
+        <div className="logo">
+          <a href="#home">
+            <span className="logo-text">DevOps ∞ Journey</span>
+          </a>
+        </div>
+      </nav>
+      <nav className="nav">
+        <ul className="nav-links">
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+        <div className="hamburger">
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
